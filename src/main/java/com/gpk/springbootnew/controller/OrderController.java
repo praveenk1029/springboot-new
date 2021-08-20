8ce03493deee188110dd9545c09c9ad6ce95a723
praveenk1029 <praveenk1029@gmail.com>
2021-08-20 00:31:47 -0400

    Order list updated.

--- src/main/java/com/gpk/springbootnew/controller/OrderController.java
+++ src/main/java/com/gpk/springbootnew/controller/OrderController.java
@@ -2,10 +2,18 @@ package com.gpk.springbootnew.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
 public class OrderController {
     @RequestMapping(value="/getOrder", method = RequestMethod.GET)
-    public String getOrder(){
-        return "Order# 2482...";
+    public List<String> getOrder(){
+        List<String> orders = Arrays.asList("Electronics", "Office Supplies", "Toys");
+        orders.stream().forEach(System.out::println);
+        return orders.stream().collect(Collectors.toList());
     }
 }
