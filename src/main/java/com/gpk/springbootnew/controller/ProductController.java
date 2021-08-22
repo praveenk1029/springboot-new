f168a7f3123151ec0a9f2227a43ee0ea5d5cd8ae
praveenk1029 <praveenk1029@gmail.com>
2021-08-22 00:45:21 -0400

    Added checkout info.

--- src/main/java/com/gpk/springbootnew/controller/ProductController.java
+++ src/main/java/com/gpk/springbootnew/controller/ProductController.java
@@ -9,6 +9,9 @@ import java.util.List;
 @RestController
 public class ProductController {
 
+    public static final String ACCESSORIES1 = "Accessories";
+    public static final String ACCESSORIES = "Accessories";
+
     @RequestMapping(value="/getProduct", method = RequestMethod.GET)
     public String getProduct(){
         return "Tesla...";
@@ -16,10 +19,20 @@ public class ProductController {
 
     @RequestMapping(value="/getProducts", method = RequestMethod.GET)
     public List<Product> getProducts(){
+        Product p = new Product();
+        p.setProductId(10);
+        p.setProductName("Samsung TV");
+        p.setPrice(599.99);
+        System.out.println("Product 10: "+p);
+
         return Arrays.asList(new Product(1, "iPhone",999.99, "Mobile"),
                 new Product(2, "MacBook",2999.99, "Computer"),
-                new Product(4, "Apple TV",99.99, "Accessories"),
-                new Product(3, "Apple Watch",599.99, "Watch"));
+                new Product(3, "Apple TV",99.99, "Accessories"),
+                new Product(4, "Apple TV",99.99, ACCESSORIES),
+                new Product(5, "Apple TV",99.99, ACCESSORIES),
+                new Product(6, "Apple TV",99.99, ACCESSORIES),
+                new Product(7, "Apple Watch",599.99, "Watch"),
+                new Product(8, "Apple Watch",599.99, "Watch"));
     }
 
 }
