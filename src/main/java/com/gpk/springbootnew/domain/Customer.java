22af7a17d5dedd7b133c3979f1dfcfaf5188e25d
praveenk1029 <praveenk1029@gmail.com>
2021-08-22 22:33:04 -0400

    Customer file added.

---
+++ src/main/java/com/gpk/springbootnew/domain/Customer.java
@@ -0,0 +1,43 @@
+package com.gpk.springbootnew.domain;
+
+import java.util.Objects;
+
+public class Customer {
+    private long customerId;
+    private String name;
+    private int age;
+    private String city;
+
+    public Customer(long customerId, String name, int age, String city) {
+        this.customerId = customerId;
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+    public long getCustomerId() {
+        return customerId;
+    }
+    public void setCustomerId(long customerId) {
+        this.customerId = customerId;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public Customer() {
+    }
+}
